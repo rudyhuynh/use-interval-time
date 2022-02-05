8b28eca7e61eed2518b9fe5a870f2c2953dee71a
Rudy Huynh <rudyhuynh@niometrics.com>
2022-02-05 14:16:16 +0800

    typescript library template

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export function useIntervalTime(intervalMs: number = 1000): Date {
+  const [time, setTime] = useState(() => new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, intervalMs);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return time;
+}
