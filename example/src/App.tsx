720d68fd8af3a1fe44ac33c2fac08599bb66162c
Rudy Huynh <rudy.huynh286@gmail.com>
2023-03-09 22:32:55 +0700

    remove example; use jsdom for testEnvironment

--- example/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import { useIntervalTime } from "use-interval-time";
-
-function App() {
-  return <div className="App">Time: {useIntervalTime().toISOString()}</div>;
-}
-
-export default App;
